aa2936a5a9352785bd1e16e4d5a157b3f2128126
ingrid <ingrid.oltmann.liz@gmail.com>
2025-11-10 08:57:44 -0300

    primer commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+let nombre = "Ingrese su nombre"
+console.log(nombre)
+
+
+let contraseña = "Contraseña"
+console.log(contraseña)
+
+let titulo = "ArteHabitar";
+console.log(titulo);
+
+let nullValue = null
+
+console.log(typeof nombre)
+
+let myEmail = "ingrid.oltmann.liz@gmail.com"
+
+let edad = 40
+
+let edadPermitida = "true"
+let menorDeEdad = "false"
+
+    if (edad > 18){ 
+        console.log(edadPermitida);
+    }
+            else{
+                console.log(menorDeEdad)
+            }
